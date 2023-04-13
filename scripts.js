9a1984b7f2feda1b29c7231dfbd26e667b13875b
Home <home@MacBook-Pro-Egor.local>
2023-04-13 08:14:11 +0300

    mode style to iframe loader

--- scripts.js
+++ scripts.js
@@ -1,26 +1,42 @@
-const INSTANCE_NAME = "billingWidget";
-const APP_URL = "https://test-react-widget.vercel.app/";
-// const APP_URL = "http://localhost:4000/";
+(function () {
+  const INSTANCE_NAME = "billingWidget";
+  const APP_URL = "https://test-react-widget.vercel.app/";
+  // const APP_URL = "http://localhost:4000/";
 
-const guid = () => {
-  var S4 = () => (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-  return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
-};
+  const guid = () => {
+    var S4 = () => (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+  };
 
-let appLoading;
+  let appLoading;
 
-const loadApp = (payUrl, id) => {
-  if(!appLoading) {
-    appLoading = true;
-    
-    const spinKeyframe = `spin-keyframe-${id}`;
-    const noScrollClass = `noscroll-${id}`;
-    document.body.classList.add(noScrollClass);
+  const loadApp = (payUrl, id) => {
+    if (!appLoading) {
+      appLoading = true;
 
-    const loadingDiv = document.createElement('div');
-    const spinner = document.createElement('div');
+      const spinKeyframe = `spin-keyframe-${id}`;
+      const noScrollClass = `noscroll-${id}`;
+      document.body.classList.add(noScrollClass);
 
-    loadingDiv.style.cssText = `
+      const style = document.querySelector(`style#style-${id}`);
+      if (!style) {
+        document.head.insertAdjacentHTML("beforeend", `
+          <style id=style-${id}>
+            .${noScrollClass} {
+              overflow: hidden;
+            }
+            @keyframes ${spinKeyframe} {
+              to {
+                -webkit-transform: rotate(360deg);
+              }
+            };
+          </style>
+        `);
+      };
+
+      const loadingDiv = document.createElement('div');
+      const spinner = document.createElement('div');
+      loadingDiv.style.cssText = `
       height: 100% !important; 
       width: 100% !important; 
       position: fixed !important;
@@ -29,7 +45,7 @@ const loadApp = (payUrl, id) => {
       z-index: 99998 !important;
       background: rgba(33, 33, 58, 0.54);
     `;
-    spinner.style.cssText = `
+      spinner.style.cssText = `
       display: block;
       width: 34px;
       height: 34px;
@@ -45,13 +61,13 @@ const loadApp = (payUrl, id) => {
       margin: auto;
     `;
 
-    const iframe = document.createElement('iframe');
-    iframe.onload = () => {
-      appLoading = false;
-    };
-    iframe.src = APP_URL;
-    iframe.name = id;
-    iframe.style.cssText = `
+      const iframe = document.createElement('iframe');
+      iframe.onload = () => {
+        appLoading = false;
+      };
+      iframe.src = APP_URL;
+      iframe.name = id;
+      iframe.style.cssText = `
       height: 100% !important;
       width: 100% !important; 
       position: fixed !important; 
@@ -61,107 +77,88 @@ const loadApp = (payUrl, id) => {
       z-index: 99999 !important;
     `;
 
-    const receiveMessage = (e) => {
-      const type = e?.data?.billing_widget?.type;
-      switch (type) {
-        case `loaded-${id}`:
-          spinner.remove();
-          break;
-        case `close-iframe-${id}`:
-          window.removeEventListener("message", receiveMessage);
-          iframe.remove();
-          loadingDiv.remove();
-          document.body.classList.remove(noScrollClass);
-          break;
-        case `get-data-${id}`:
-          iframe.contentWindow.postMessage(
-            {
-              billing_widget: {
-                type: `send-data-${id}`,
-                data: {
-                  payUrl,
-                }
+      const receiveMessage = (e) => {
+        const type = e?.data?.billing_widget?.type;
+        switch (type) {
+          case `loaded-${id}`:
+            spinner.remove();
+            break;
+          case `close-iframe-${id}`:
+            window.removeEventListener("message", receiveMessage);
+            iframe.remove();
+            loadingDiv.remove();
+            document.body.classList.remove(noScrollClass);
+            break;
+          case `get-data-${id}`:
+            iframe.contentWindow.postMessage(
+              {
+                billing_widget: {
+                  type: `send-data-${id}`,
+                  data: {
+                    payUrl,
+                  }
+                },
               },
-            },
-            APP_URL
-          );
-          break;
-        default:
-          console.warn(`Unsupported [${type}]`);
+              APP_URL
+            );
+            break;
+          default:
+            console.warn(`Unsupported [${type}]`);
+        }
+      }
+      window.addEventListener("message", receiveMessage);
+
+      loadingDiv.appendChild(spinner);
+      document.body.appendChild(loadingDiv);
+      document.body.appendChild(iframe);
+    };
+  };
+
+  const loader = (win, script) => {
+    const instanceLoader = win[INSTANCE_NAME];
+    const id = guid();
+
+    if (instanceLoader?.q) {
+      // async init
+      for (let i = 0; i < instanceLoader?.q?.length; i++) {
+        const item = instanceLoader.q[i];
+        const method = item[0];
+        const args = item[1];
+
+        switch (method) {
+          case 'init':
+            loadApp(args.url, id);
+            break;
+          default:
+            console.warn(`Unsupported [${method}]`, args);
+        }
       }
     }
-    window.addEventListener("message", receiveMessage);
 
-    loadingDiv.appendChild(spinner);
-    document.body.appendChild(loadingDiv);
-    document.body.appendChild(iframe);
-  };
-};
-
-const loader = (win, script) => {
-  const instanceLoader = win[INSTANCE_NAME];
-  const id = guid();
-  const spinKeyframe = `spin-keyframe-${id}`;
-  const noScrollClass = `noscroll-${id}`
-
-  const style = document.querySelector(`style#style-${id}`);
-  if (!style) {
-    document.head.insertAdjacentHTML("beforeend", `
-      <style id=style-${id}>
-        .${noScrollClass} {
-          overflow: hidden;
-        }
-        @keyframes ${spinKeyframe} {
-          to {
-            -webkit-transform: rotate(360deg);
-          }
-        };
-      </style>
-    `);
-  };
-
-  if (instanceLoader?.q) {
-    // async init
-    for (let i = 0; i < instanceLoader?.q?.length; i++) {
-      const item = instanceLoader.q[i];
-      const method = item[0];
-      const args = item[1];
-
+    // call 
+    win[INSTANCE_NAME] = (method, args) => {
       switch (method) {
-        case 'init':
+        case 'init': {
           loadApp(args.url, id);
           break;
+        }
         default:
           console.warn(`Unsupported [${method}]`, args);
       }
-    }
-  }
+    };
 
-  // call 
-  win[INSTANCE_NAME] = (method, args) => {
-    switch (method) {
-      case 'init': {
-        loadApp(args.url, id);
-        break;
+    const staticElementsID = script.getAttribute("data-payment-id") || `${INSTANCE_NAME}ID`;
+    const staticElements = window.document.querySelectorAll(`[id=${staticElementsID}]`);
+    if (staticElements.length) {
+      const DATA_URL_ATTR = "data-payment-url";
+      for (let i = 0; i < staticElements?.length; i++) {
+        const element = staticElements[i];
+        const url = element.getAttribute(DATA_URL_ATTR);
+        if (url) {
+          element.addEventListener("click", () => win[INSTANCE_NAME]("init", { url }));
+        }
       }
-      default:
-        console.warn(`Unsupported [${method}]`, args);
     }
   };
-
-  const staticElementsID = script.getAttribute("data-payment-id") || `${INSTANCE_NAME}ID`;
-  const staticElements = window.document.querySelectorAll(`[id=${staticElementsID}]`);
-  const scriptID = script.getAttribute("id");
-  if (staticElements.length && !scriptID) {
-    const DATA_URL_ATTR = "data-payment-url";
-    for (let i = 0; i < staticElements?.length; i++) {
-      const element = staticElements[i];
-      const url = element.getAttribute(DATA_URL_ATTR);
-      if (url) {
-        element.addEventListener("click", () => win[INSTANCE_NAME]("init", { url }));
-      }
-    }
-  }
-};
-
-loader(window, document.currentScript);
+  loader(window, document.currentScript);
+})();
